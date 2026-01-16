6a6d0b47c7b50b1d8b29d128438a6dddb8a8d1ef
Jos <josephine.leow@razer.com>
2026-01-16 16:14:24 +0800

    Initial React notes app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Josephine Leow's React Notes</h1>
+      <p>My first React app is running 🚀</p>
+    </div>
+  );
+}
